88446d1634dcd9f96747c1f332639240dd041e0f
chinn <chinn@CHINNA>
2025-03-06 10:04:44 +0530

    MUI to Chatty changed

--- Client/src/pages/header/Header.jsx
+++ Client/src/pages/header/Header.jsx
@@ -173,7 +173,7 @@ export default function PrimarySearchAppBar() {
             component="div"
             sx={{ display: { xs: "none", sm: "block" } }}
           >
-            MUI
+            Chatty
           </Typography>
           <Search>
             <SearchIconWrapper>
